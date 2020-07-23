6ccb7aa8ee6fc3ad1a5b38b7163107576d94d0f1
basuha <bassturbatorark@gmail.com>
2020-07-23 13:39:50 +0600

    #12 complete

--- src/main/java/letscode/sarfan/config/MvcConfig.java
+++ src/main/java/letscode/sarfan/config/MvcConfig.java
@@ -1,12 +1,25 @@
 package letscode.sarfan.config;
 
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerCustomizer() {
+        return container -> {
+            container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,"/"));
+        };
+    }
+
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
     }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuetify from "vuetify";
 import '@babel/polyfill'
 import 'api/resource'
+import router from 'router/router'
 import App from 'pages/App.vue'
 import store from "store/store";
 import {connect} from './util/ws.js'
@@ -16,7 +17,7 @@ Vue.use(Vuetify)
 new Vue({
     el: '#app',
     store: store,
+    router: router,
     render: a => a(App),
     vuetify: new Vuetify({})
-
 })
