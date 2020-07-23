397e2c1e0e14891e9ea3fa4c1e223e77de895e72
basuha <bassturbatorark@gmail.com>
2020-07-23 16:08:12 +0600

    #13 complete

--- src/main/java/letscode/sarfan/controller/MessageController.java
+++ src/main/java/letscode/sarfan/controller/MessageController.java
@@ -4,20 +4,34 @@ import com.fasterxml.jackson.annotation.JsonView;
 import letscode.sarfan.domain.Message;
 import letscode.sarfan.domain.Views;
 import letscode.sarfan.dto.EventType;
+import letscode.sarfan.dto.MetaDto;
 import letscode.sarfan.dto.ObjectType;
 import letscode.sarfan.repo.MessageRepo;
 import letscode.sarfan.util.WsSender;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("message")
 public class MessageController {
+    private static String URL_PATTERN = "https?:\\/\\/?[\\w\\d\\._\\-%\\/\\?=&#]+";
+    private static String IMAGE_PATTERN = "\\.(jpeg|jpg|gif|png)$";
+
+    private static Pattern URL_REGEX = Pattern.compile(URL_PATTERN, Pattern.CASE_INSENSITIVE);
+    private static Pattern IMG_REGEX = Pattern.compile(IMAGE_PATTERN, Pattern.CASE_INSENSITIVE);
+
     private final MessageRepo messageRepo;
     private final BiConsumer<EventType, Message> wsSender;
 
@@ -40,12 +54,11 @@ public class MessageController {
     }
 
     @PostMapping
-    public Message create(@RequestBody Message message) {
+    public Message create(@RequestBody Message message) throws IOException {
         message.setCreationDate(LocalDateTime.now());
+        fillMeta(message);
         Message updatedMessage = messageRepo.save(message);
-
         wsSender.accept(EventType.CREATE, updatedMessage);
-
         return updatedMessage;
     }
 
@@ -53,13 +66,12 @@ public class MessageController {
     public Message update(
             @PathVariable("id") Long messageFromDbId,
             @RequestBody Message changedMessage
-    ) {
+    ) throws IOException {
         Message messageFromDb = messageRepo.findById(messageFromDbId).get();
         BeanUtils.copyProperties(changedMessage, messageFromDb, "id");
-
+        fillMeta(messageFromDb);
         Message updatedMessage = messageRepo.save(messageFromDb);
         wsSender.accept(EventType.UPDATE, updatedMessage);
-
         return updatedMessage;
     }
 
@@ -68,5 +80,44 @@ public class MessageController {
         Message messageForDelete = messageRepo.getOne(messageid);
         messageRepo.delete(messageForDelete);
         wsSender.accept(EventType.REMOVE, messageForDelete);
+    }
+
+    private void fillMeta(Message message) throws IOException {
+        String text = message.getText();
+        Matcher matcher = URL_REGEX.matcher(text);
+
+
+        if (matcher.find()) {
+            String url = text.substring(matcher.start(), matcher.end());
+            matcher = IMG_REGEX.matcher(url);
+            message.setLink(url);
+
+            if (matcher.find()) {
+                message.setLinkCover(url);
+            } else if (!url.contains("youtu")) {
+                MetaDto meta = getMeta(url);
+                message.setLinkTitle(meta.getTitle());
+                message.setLinkDescription(meta.getDescription());
+                message.setLinkCover(meta.getCover());
+            }
+        }
+    }
+
+    private MetaDto getMeta(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+
+        Elements title = doc.select("meta[name$=title], meta[property$=title]");
+        Elements description = doc.select("meta[name$=description], meta[property$=description]");
+        Elements cover = doc.select("meta[name$=image], meta[property$=image]");
+
+        return new MetaDto(
+                getContent(title.first()),
+                getContent(description.first()),
+                getContent(cover.first())
+        );
+    }
+
+    private String getContent(Element element) {
+        return element == null ? "" : element.attr("content");
     }
 }
--- src/main/java/letscode/sarfan/domain/Message.java
+++ src/main/java/letscode/sarfan/domain/Message.java
@@ -2,10 +2,7 @@ package letscode.sarfan.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonView;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.swing.text.View;
@@ -13,19 +10,32 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table
-@ToString
 @EqualsAndHashCode(of = {"id"})
-@Getter
-@Setter
+@Data
 public class Message {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @JsonView(Views.Id.class)
     private Long id;
+
     @JsonView(Views.IdName.class)
     private String text;
+
     @Column(updatable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonView(Views.FullMessage.class)
     private LocalDateTime creationDate;
+
+    @JsonView(Views.FullMessage.class)
+    private String link;
+
+    @JsonView(Views.FullMessage.class)
+    private String linkTitle;
+
+    @JsonView(Views.FullMessage.class)
+    private String linkDescription;
+
+    @JsonView(Views.FullMessage.class)
+    private String linkCover;
 }
